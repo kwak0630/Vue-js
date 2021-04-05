3167e1b827e2a80466e45fee33bfc87930f2d762
mason <meju7015@gmail.com>
2021-04-05 17:28:40 +0900

    Fix 문제점
    1. boardListItem 은 모달에 뿌려줄 데이터가 아니라 리스트를 만들기 위한 모델입니다
    
    2. 클릭할때마다 클릭된 객체가 전달될 modalData 변수를 만들고 이 변수를 모달에 props로 전달한다

--- 5th/src/components/common/modal.vue
+++ 5th/src/components/common/modal.vue
@@ -1,150 +1,144 @@
 <template>
-	<transition>
-		<div class="modal-wrap"
-			 v-if="visible"
-			 v-bind:item="boardListItem"
-			 v-bind:visible="visible">
-			<!-- v-bind:list=:boardListItem"-->
-			<div class="modal-inner">
-				<div class="modal-content">
-					<button type="button" @click="close" class="close">닫기</button>
-					<div class="modal-header">
-						<h3 class="subject">제목{{ subject }}</h3>
-						<div class="info">
-							<span>작성자{{ name }}</span>
-							<span>날짜{{ date }}</span>
-						</div>
-					</div>
-					<div class="modal-body">
-						<div class="thumb">이미지{{ imgsrc }}</div>
-						<p class="description">설명{{ description }}</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</transition>
+  <transition>
+    <div class="modal-wrap"
+         v-if="visible">
+      <!-- v-bind:list=:boardListItem"-->
+      <div class="modal-inner">
+        <div class="modal-content">
+          <button type="button" @click="close" class="close">닫기</button>
+          <div class="modal-header">
+            <h3 class="subject">제목{{ modalData.subject }}</h3>
+            <div class="info">
+              <span>작성자{{ modalData.name }}</span>
+              <span>날짜{{ modalData.date }}</span>
+            </div>
+          </div>
+          <div class="modal-body">
+            <div class="thumb">이미지{{ modalData.imgsrc }}</div>
+            <p class="description">설명{{ modalData.description }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
 </template>
 
 
 <script>
-	export default {
-		name: 'ModalView',
-		props: [
-			'visible',
-			'subject',
-			'name',
-			'date',
-			'imgsrc',
-			'description'
-		],
-		methods: {
-			close: function () {
-				this.$emit('close');
-			},
-		}
-	};
+export default {
+  name: 'ModalView',
+  props: [
+    'visible',
+    'modalData'
+  ],
+  methods: {
+    close: function () {
+      this.$emit('close');
+    },
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-	.modal-wrap{
-		&:before{
-			content:'';
-			position:fixed;top:0;left:0;
-			width:100%;height:100%;
-			background:rgba(0, 0, 0, 0.65);
-		}
-		.modal-inner{
-			position:fixed;top:50%;left:50%;
-			transform:translate(-50%, -50%);
-			width:500px;
-			background:#f7f7f7;
-			border-radius:15px;
-			padding:60px 30px 30px;
-		}
-		.modal-content{
-			overflow-y:scroll;
-			height:400px;
-			text-align:left;
-			&::-webkit-scrollbar{
-				display:none;
-			}
-			.modal-header{
-				position:relative;
-				margin-bottom:35px;
-			}
-			.subject{
-				padding:10px 0 15px;
-				font-size:30px;
-				font-weight:700;
-				border-bottom:1px solid #ddd;
-			}
-			.info{
-				position:absolute;
-				bottom:25px;
-				right:5px;
-				font-size:0;
-				span{
-					position:relative;
-					display:inline-block;
-					font-size:13px;
-					padding-right:10px;
-					color:#999999;
-					&:after{
-						display:inline-block;
-						content:'';
-						margin-left:10px;
-						width:1px;
-						height:10px;
-						background:#ddd;
-						vertical-align:middle;
-					}
-					&:last-child{
-						padding:0;
-						&:after{
-							display:none
-						}
-					}
-				}
-			}
-			.thumb{
-			}
-			.description{
-				margin-top:20px;
-				line-height:21px;
-			}
-		}
-		.close{
-			display:inline-block;
-			position:absolute;top:20px;right:20px;
-			width:30px; height:30px;
-			background:transparent;
-			border:1px solid #222;
-			border-radius:100%;
-			font-size:0;
-			&:before, &:after{
-				content:'';
-				position:absolute;
-				top:50%;
-				left:50%;
-				width:16px;
-				height:1px;
-				background-color:#222;
-			}
-			&:before{
-				transform:translate(-50%, -50%) rotate(45deg);
-			}
-			&:after{
-				transform:translate(-50%, -50%) rotate(135deg);
-			}
-		}
-	}
-	.modal-enter-active, .modal-leave-active {
-		transition:.5s;
-	}
-	.modal-leave-active {
-		position:absolute;
-	}
-	.modal-enter, .modal-leave-to {
-		opacity:0;
-	}
+.modal-wrap{
+  &:before{
+    content:'';
+    position:fixed;top:0;left:0;
+    width:100%;height:100%;
+    background:rgba(0, 0, 0, 0.65);
+  }
+  .modal-inner{
+    position:fixed;top:50%;left:50%;
+    transform:translate(-50%, -50%);
+    width:500px;
+    background:#f7f7f7;
+    border-radius:15px;
+    padding:60px 30px 30px;
+  }
+  .modal-content{
+    overflow-y:scroll;
+    height:400px;
+    text-align:left;
+    &::-webkit-scrollbar{
+      display:none;
+    }
+    .modal-header{
+      position:relative;
+      margin-bottom:35px;
+    }
+    .subject{
+      padding:10px 0 15px;
+      font-size:30px;
+      font-weight:700;
+      border-bottom:1px solid #ddd;
+    }
+    .info{
+      position:absolute;
+      bottom:25px;
+      right:5px;
+      font-size:0;
+      span{
+        position:relative;
+        display:inline-block;
+        font-size:13px;
+        padding-right:10px;
+        color:#999999;
+        &:after{
+          display:inline-block;
+          content:'';
+          margin-left:10px;
+          width:1px;
+          height:10px;
+          background:#ddd;
+          vertical-align:middle;
+        }
+        &:last-child{
+          padding:0;
+          &:after{
+            display:none
+          }
+        }
+      }
+    }
+    .thumb{
+    }
+    .description{
+      margin-top:20px;
+      line-height:21px;
+    }
+  }
+  .close{
+    display:inline-block;
+    position:absolute;top:20px;right:20px;
+    width:30px; height:30px;
+    background:transparent;
+    border:1px solid #222;
+    border-radius:100%;
+    font-size:0;
+    &:before, &:after{
+      content:'';
+      position:absolute;
+      top:50%;
+      left:50%;
+      width:16px;
+      height:1px;
+      background-color:#222;
+    }
+    &:before{
+      transform:translate(-50%, -50%) rotate(45deg);
+    }
+    &:after{
+      transform:translate(-50%, -50%) rotate(135deg);
+    }
+  }
+}
+.modal-enter-active, .modal-leave-active {
+  transition:.5s;
+}
+.modal-leave-active {
+  position:absolute;
+}
+.modal-enter, .modal-leave-to {
+  opacity:0;
+}
 </style>
--- 5th/src/page/boardlist.vue
+++ 5th/src/page/boardlist.vue
@@ -1,401 +1,398 @@
 <template>
-	<div>
-		<Lnb />
-		<div class="content">
-			<h2>게시판</h2>
-			<p class="sub-tit">
-				- 타입에 따라 갤러리형, 리스트형, 웹진형의 스타일을 가지는 리스트 컴포넌트를 만들어 주세요<br/>
-				1. 리스트 컴포넌트에 들어갈 Model 을 설계해야 합니다.<br/>
-				2. Model 은 갤러리형, 리스트형, 웹진형에 모두 대응할 수 있어야 합니다.<br/>
-				3. 갤러리형, 리스트형, 웹진형의 전환 버튼을 구현하여 실시간으로 변경될 수 있어야 합니다.
-			</p>
-			<div class="board-wrap">
-				<div class="board-top">
-					<!-- 리스트 타입 -->
-					<div class="list-type">
-						<a v-on:click="type='list'" :class="[ type === 'list' ? 'active' : '' ]">리스트</a>
-						<a v-on:click="type='gallery'" :class="[ type === 'gallery' ? 'active' : '' ]">갤러리</a>
-						<a v-on:click="type='webzine'" :class="[ type === 'webzine' ? 'active' : '' ]">웹진</a>
-					</div>
-					<!-- 검색 -->
-					<div class="sch-wrap">
-						<input-field
-								v-model="inputSch"
-								type="text"
-								id="boardsch"
-								placeholder="검색어를 입력하세요."
-						/>
-						<button class="btn">검색</button>
-					</div>
-				</div>
+  <div>
+    <Lnb />
+    <div class="content">
+      <h2>게시판</h2>
+      <p class="sub-tit">
+        - 타입에 따라 갤러리형, 리스트형, 웹진형의 스타일을 가지는 리스트 컴포넌트를 만들어 주세요<br/>
+        1. 리스트 컴포넌트에 들어갈 Model 을 설계해야 합니다.<br/>
+        2. Model 은 갤러리형, 리스트형, 웹진형에 모두 대응할 수 있어야 합니다.<br/>
+        3. 갤러리형, 리스트형, 웹진형의 전환 버튼을 구현하여 실시간으로 변경될 수 있어야 합니다.
+      </p>
+      <div class="board-wrap">
+        <div class="board-top">
+          <!-- 리스트 타입 -->
+          <div class="list-type">
+            <a v-on:click="type='list'" :class="[ type === 'list' ? 'active' : '' ]">리스트</a>
+            <a v-on:click="type='gallery'" :class="[ type === 'gallery' ? 'active' : '' ]">갤러리</a>
+            <a v-on:click="type='webzine'" :class="[ type === 'webzine' ? 'active' : '' ]">웹진</a>
+          </div>
+          <!-- 검색 -->
+          <div class="sch-wrap">
+            <input-field
+                v-model="inputSch"
+                type="text"
+                id="boardsch"
+                placeholder="검색어를 입력하세요."
+            />
+            <button class="btn">검색</button>
+          </div>
+        </div>
 
-				<div class="board-content">
-					<div v-if="type ==='list'" class="tab-content">
-						<div class="table-wrap">
-							<table>
-								<colgroup>
-									<col width="10%"/>
-									<col />
-									<col width="10%"/>
-									<col width="20%"/>
-								</colgroup>
-								<thead>
-								<tr>
-									<th>번호</th>
-									<th>제목</th>
-									<th>작성자</th>
-									<th>날짜</th>
-								</tr>
-								</thead>
+        <div class="board-content">
+          <div v-if="type ==='list'" class="tab-content">
+            <div class="table-wrap">
+              <table>
+                <colgroup>
+                  <col width="10%"/>
+                  <col />
+                  <col width="10%"/>
+                  <col width="20%"/>
+                </colgroup>
+                <thead>
+                <tr>
+                  <th>번호</th>
+                  <th>제목</th>
+                  <th>작성자</th>
+                  <th>날짜</th>
+                </tr>
+                </thead>
 
-								<tbody id="tableList">
-								<tr v-for="(item, key) in boardListItem" :key="key">
-									<td>{{item.num}}</td>
-									<td class="subject"><a href="#" @click="modalOpen(item)">{{item.subject}}</a></td>
-									<td>
-										{{ item.name }}
-									</td>
-									<td>
-										{{item.date}}
-									</td>
-								</tr>
-								</tbody>
-							</table>
+                <tbody id="tableList">
+                <tr v-for="(item, key) in boardListItem" :key="key">
+                  <td>{{item.num}}</td>
+                  <td class="subject"><a href="#" @click="modalOpen(item)">{{item.subject}}</a></td>
+                  <td>
+                    {{ item.name }}
+                  </td>
+                  <td>
+                    {{item.date}}
+                  </td>
+                </tr>
+                </tbody>
+              </table>
 
-							<pagination :total="5" :current-page="currentPage" @pagechanged="onPageChange"></pagination>
-						</div>
-					</div>
-					<div v-if="type ==='gallery'" class="tab-content">
-						<div class="gallery-list">
-							<ul>
-								<li v-for="(item, key) in boardListItem" :key="key">
-									<a href="#" @click="modalOpen(item)">
-										<div class="thumb">
-											<img :src="item.imgsrc" :alt="item.subject" @error="errorImg" />
-										</div>
-									</a>
-								</li>
-							</ul>
-						</div>
+              <pagination :total="5" :current-page="currentPage" @pagechanged="onPageChange"></pagination>
+            </div>
+          </div>
+          <div v-if="type ==='gallery'" class="tab-content">
+            <div class="gallery-list">
+              <ul>
+                <li v-for="(item, key) in boardListItem" :key="key">
+                  <a href="#" @click="modalOpen(item)">
+                    <div class="thumb">
+                      <img :src="item.imgsrc" :alt="item.subject" @error="errorImg" />
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
 
-						<pagination :total="5" :current-page="currentPage" @pagechanged="onPageChange"></pagination>
-					</div>
-					<div v-if="type ==='webzine'" class="tab-content">
-						<div class="gallery-list v2">
-							<ul>
-								<li v-for="(item, key) in boardListItem" :key="key">
-									<a href="#" @click="modalOpen(item)">
-										<div class="thumb">
-											<img :src="item.imgsrc" @error="errorImg" />
-										</div>
-										<div class="cont">
-											<p class="subject">{{item.subject}}</p>
-											<p class="description">{{item.description}}</p>
-											<p class="info">
-												<span>{{item.name}}</span>
-												<span>{{item.date}}</span>
-											</p>
-										</div>
-									</a>
-								</li>
-							</ul>
-						</div>
+            <pagination :total="5" :current-page="currentPage" @pagechanged="onPageChange"></pagination>
+          </div>
+          <div v-if="type ==='webzine'" class="tab-content">
+            <div class="gallery-list v2">
+              <ul>
+                <li v-for="(item, key) in boardListItem" :key="key">
+                  <a href="#" @click="modalOpen(item)">
+                    <div class="thumb">
+                      <img :src="item.imgsrc" @error="errorImg" />
+                    </div>
+                    <div class="cont">
+                      <p class="subject">{{item.subject}}</p>
+                      <p class="description">{{item.description}}</p>
+                      <p class="info">
+                        <span>{{item.name}}</span>
+                        <span>{{item.date}}</span>
+                      </p>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
 
-						<pagination :total="5" :current-page="currentPage" @pagechanged="onPageChange"></pagination>
-					</div>
+            <pagination :total="5" :current-page="currentPage" @pagechanged="onPageChange"></pagination>
+          </div>
 
-					<Modal-View
-							v-bind:visible="visible"
-							v-on:close="modalClose"
-					/>
-					<!-- v-bind:list=:boardListItem"-->
-				</div>
-			</div>
-		</div>
-		<Footer />
-	</div>
+          <modal-view
+              v-bind:modalData="modalData"
+              v-bind:visible="visible"
+              v-on:close="modalClose"
+          />
+          <!-- v-bind:list=:boardListItem"-->
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
 </template>
 
 <script>
-	import Lnb from "@/components/layout/Lnb";
-	import Footer from "@/components/layout/Footer";
-	import InputField from "@/components/form/inputField";
-	import Tabs from "@/components/common/tab";
-	import Pagination from "@/components/board/pagination";
-	import ModalView from "@/components/common/modal";
-	import img from "@/assets/nodata.png"; //이미지 데이터 없을 경우
+import Lnb from "@/components/layout/Lnb";
+import Footer from "@/components/layout/Footer";
+import InputField from "@/components/form/inputField";
+import Tabs from "@/components/common/tab";
+import Pagination from "@/components/board/pagination";
+import ModalView from "@/components/common/modal";
+import img from "@/assets/nodata.png"; //이미지 데이터 없을 경우
 
-	export default {
-		name: "boardlist",
-		components:	{
-			Lnb,
-			Footer,
-			InputField,
-			Pagination,
-			ModalView,
-		},
-		data() {
-			return {
-				inputSch: '',
-				type: 'list',
-				visible: false,
-				currentPage: 1,
-				boardListItem: [
-					{
-						num: '1',
-						subject: '공지사항입니다.',
-						description: '공지사항입니다. 공지사항입니다. 공지사항입니다. 공지사항입니다. 공지사항입니다.',
-						name: 'sally',
-						date : '2021-04-02',
-						class: 'notice',
-						imgsrc: require('@/assets/img02.png'),
-					},
-					{
-						num: '2',
-						subject: '4/3(토) 2021 프로야구 개막',
-						description: '드디어 야구 시즌 시작!',
-						name: 'sally',
-						date : '2021-04-02',
-						imgsrc: require('@/assets/img01.jpg'),
-					},
-					{
-						num: '3',
-						subject: '제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 ',
-						description: '내용 내용 내용 내용 내용 내용',
-						name: 'sally',
-						date: '2021-04-01',
-						imgsrc: '@/assets/logo.png',
-					}
-				],
-			}
-		},
-		methods: {
-			onPageChange: function (page) {
-				//console.log(page)
-				this.currentPage = page;
-			},
-			errorImg(e){
-				/*
-				이미지 오류일 경우 @error=errorImg
-				이미지 imfort 해서 가져오
-				*/
-				e.target.src = img;
-			},
+export default {
+  name: "boardlist",
+  components:	{
+    Lnb,
+    Footer,
+    InputField,
+    Pagination,
+    ModalView,
+  },
+  data() {
+    return {
+      inputSch: '',
+      type: 'list',
+      visible: false,
+      currentPage: 1,
+      boardListItem: [
+        {
+          num: '1',
+          subject: '공지사항입니다.',
+          description: '공지사항입니다. 공지사항입니다. 공지사항입니다. 공지사항입니다. 공지사항입니다.',
+          name: 'sally',
+          date : '2021-04-02',
+          class: 'notice',
+          imgsrc: require('@/assets/img02.png'),
+        },
+        {
+          num: '2',
+          subject: '4/3(토) 2021 프로야구 개막',
+          description: '드디어 야구 시즌 시작!',
+          name: 'sally',
+          date : '2021-04-02',
+          imgsrc: require('@/assets/img01.jpg'),
+        },
+        {
+          num: '3',
+          subject: '제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 제목 ',
+          description: '내용 내용 내용 내용 내용 내용',
+          name: 'sally',
+          date: '2021-04-01',
+          imgsrc: '@/assets/logo.png',
+        }
+      ],
+      modalData: {},
+    }
+  },
+  methods: {
+    onPageChange: function (page) {
+      //console.log(page)
+      this.currentPage = page;
+    },
+    errorImg(e){
+      /*
+      이미지 오류일 경우 @error=errorImg
+      이미지 imfort 해서 가져오
+      */
+      e.target.src = img;
+    },
 
-			modalOpen: function(item) {
-				//this.boardListItem = item;
-				this.subject = item.subject;
-				this.name = item.name;
-				this.date = item.date;
-				this.description = item.description;
-				this.imgsrc = item.imgsrc;
-				this.visible = true;
-			},
+    modalOpen: function(item) {
+      this.modalData = item;
+      this.visible = true;
+    },
 
-			modalClose() {
-				this.visible = false;
-			}
+    modalClose() {
+      this.visible = false;
+    }
 
-		}
-	}
+  }
+}
 </script>
 
 <style lang="scss">
-	.board-wrap{
-		margin-top:50px;
-		.board-top{
-			position:relative;
-			padding-bottom:40px;
-			.list-type{
-				a{
-					margin-right:15px;
-					&.active{
-						color:#FF8F00;
-						font-weight:700;
-					}
-				}
-			}
-		}
-		.board-content{
-			.table-wrap{
-				width:100%;
-				table {
-					border-top:2px solid #ff8f00;
-					th, td {
-						padding: 20px 0;
-						border-bottom: 1px solid #DDD;
-						text-align: center;
-						font-size: 16px;
-						vertical-align: middle;
-					}
+.board-wrap{
+  margin-top:50px;
+  .board-top{
+    position:relative;
+    padding-bottom:40px;
+    .list-type{
+      a{
+        margin-right:15px;
+        &.active{
+          color:#FF8F00;
+          font-weight:700;
+        }
+      }
+    }
+  }
+  .board-content{
+    .table-wrap{
+      width:100%;
+      table {
+        border-top:2px solid #ff8f00;
+        th, td {
+          padding: 20px 0;
+          border-bottom: 1px solid #DDD;
+          text-align: center;
+          font-size: 16px;
+          vertical-align: middle;
+        }
 
-					tr.notice {
-						background: #F8F8F8;
-						td:first-child {
-							color:#ff8f00;
-							font-weight: 700;
-						}
-					}
-					thead{
-						th{
-							background:#f7f7f7;
-						}
-					}
-					tbody{
-						tr{
-							&:hover{
-								background:#fff18252;
-							}
-						}
-						td{
-							&.subject{
-								text-align:left;
-								padding:0 10px;
-								font-size: 16px;
-							}
-						}
-					}
-				}
-			}
-			.gallery-list{
-				font-size:0;
-				li{
-					position:relative;
-					display:inline-block;
-					width:calc(33.3% - 23px);
-					min-height:300px;
-					border:1px solid #ddd;
-					vertical-align:top;
-					transition:all 0.3s ease-in;
-					&:nth-child(n+2){
-						margin-left:35px;
-					}
-					img{
-						width:100%;
-						height:100%
-					}
-					.cont{
-						//opacity:0;
-						position:absolute;
-						top:20px;
-						left:20px;
-						right:20px;
-						transition:all 0.3s ease-in;
-						p{
-							//color:#fff;
-							font-size:22px;
-						}
-					}
-					&:before{
-						transition:all 0.3s ease-in;
-					}
-					&:hover{
-						border:1px solid #FF8F00;
-						.cont{
-							//opacity: 1;
-						}
-						&:before{
-							transition:all 0.3s ease-in;
-							display:none;
-							content:'';
-							position:absolute;
-							top:0;
-							left:0;
-							width:100%;
-							height:100%;
-							background:rgba(0,0,0,0.4);
-						}
-					}
-				}
-				&.v2{
-					li{
-						width:calc(25% - 15px);
-						min-height:480px;
-						.cont{
-							opacity: 1;
-							border-top:1px solid #ddd;
-							position:relative;
-							top:inherit;
-							left:inherit;
-							right:inherit;
-							padding:15px;
-							p{
-								font-size:16px;
-								color:#333;
-							}
-							.subject{
-								margin-bottom:10px;
-								font-weight:700;
-							}
-							.description{
-								margin-bottom:10px;
-								line-height:18px;
-							}
-							.info{
-								font-size:0;
-								span{
-									position:relative;
-									display:inline-block;
-									font-size:13px;
-									padding-right:10px;
-									color:#999999;
-									&:after{
-										display:inline-block;
-										content:'';
-										margin-left:10px;
-										width:1px;
-										height:10px;
-										background:#ddd;
-										vertical-align:middle;
-									}
-									&:last-child{
-										padding:0;
-										&:after{
-											display:none
-										}
-									}
-								}
-							}
-						}
-						&:before{
-							display:none;
-						}
-					}
-				}
-			}
-		}
-		.sch-wrap{
-			position:absolute;
-			bottom:25px;
-			right:0;
-			display:block;
-			width:240px;
-			.form-item{
-				display:inline-block;
-				width:100%;
-				input{
-					padding:0 50px 0 20px;
-					height:40px;
-					line-height:40px;
-					background:#f5f5f5;
-					border-radius:20px;
-					border:0;
-				}
-			}
-			.btn{
-				position:absolute;
-				top:50%;
-				right:15px;
-				display:inline-block;
-				width:20px;
-				height:20px;
-				border-radius:3px;
-				vertical-align:middle;
-				background:transparent url("~@/assets/btn-sch.png") 0 0 no-repeat;
-				background-size:100%;
-				transform:translateY(-50%);
-				font-size:0;
-			}
-		}
-	}
+        tr.notice {
+          background: #F8F8F8;
+          td:first-child {
+            color:#ff8f00;
+            font-weight: 700;
+          }
+        }
+        thead{
+          th{
+            background:#f7f7f7;
+          }
+        }
+        tbody{
+          tr{
+            &:hover{
+              background:#fff18252;
+            }
+          }
+          td{
+            &.subject{
+              text-align:left;
+              padding:0 10px;
+              font-size: 16px;
+            }
+          }
+        }
+      }
+    }
+    .gallery-list{
+      font-size:0;
+      li{
+        position:relative;
+        display:inline-block;
+        width:calc(33.3% - 23px);
+        min-height:300px;
+        border:1px solid #ddd;
+        vertical-align:top;
+        transition:all 0.3s ease-in;
+        &:nth-child(n+2){
+          margin-left:35px;
+        }
+        img{
+          width:100%;
+          height:100%
+        }
+        .cont{
+          //opacity:0;
+          position:absolute;
+          top:20px;
+          left:20px;
+          right:20px;
+          transition:all 0.3s ease-in;
+          p{
+            //color:#fff;
+            font-size:22px;
+          }
+        }
+        &:before{
+          transition:all 0.3s ease-in;
+        }
+        &:hover{
+          border:1px solid #FF8F00;
+          .cont{
+            //opacity: 1;
+          }
+          &:before{
+            transition:all 0.3s ease-in;
+            display:none;
+            content:'';
+            position:absolute;
+            top:0;
+            left:0;
+            width:100%;
+            height:100%;
+            background:rgba(0,0,0,0.4);
+          }
+        }
+      }
+      &.v2{
+        li{
+          width:calc(25% - 15px);
+          min-height:480px;
+          .cont{
+            opacity: 1;
+            border-top:1px solid #ddd;
+            position:relative;
+            top:inherit;
+            left:inherit;
+            right:inherit;
+            padding:15px;
+            p{
+              font-size:16px;
+              color:#333;
+            }
+            .subject{
+              margin-bottom:10px;
+              font-weight:700;
+            }
+            .description{
+              margin-bottom:10px;
+              line-height:18px;
+            }
+            .info{
+              font-size:0;
+              span{
+                position:relative;
+                display:inline-block;
+                font-size:13px;
+                padding-right:10px;
+                color:#999999;
+                &:after{
+                  display:inline-block;
+                  content:'';
+                  margin-left:10px;
+                  width:1px;
+                  height:10px;
+                  background:#ddd;
+                  vertical-align:middle;
+                }
+                &:last-child{
+                  padding:0;
+                  &:after{
+                    display:none
+                  }
+                }
+              }
+            }
+          }
+          &:before{
+            display:none;
+          }
+        }
+      }
+    }
+  }
+  .sch-wrap{
+    position:absolute;
+    bottom:25px;
+    right:0;
+    display:block;
+    width:240px;
+    .form-item{
+      display:inline-block;
+      width:100%;
+      input{
+        padding:0 50px 0 20px;
+        height:40px;
+        line-height:40px;
+        background:#f5f5f5;
+        border-radius:20px;
+        border:0;
+      }
+    }
+    .btn{
+      position:absolute;
+      top:50%;
+      right:15px;
+      display:inline-block;
+      width:20px;
+      height:20px;
+      border-radius:3px;
+      vertical-align:middle;
+      background:transparent url("~@/assets/btn-sch.png") 0 0 no-repeat;
+      background-size:100%;
+      transform:translateY(-50%);
+      font-size:0;
+    }
+  }
+}
 </style>
