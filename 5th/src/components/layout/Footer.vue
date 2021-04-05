e571201f7f132ab7d285f30cf3f65c0da66b26c7
Sally kwak <39579205+kwak0630@users.noreply.github.com>
2021-04-05 17:20:24 +0900

    Add files via upload

---
+++ 5th/src/components/layout/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+	<footer>
+		<p>Vue project</p>
+	</footer>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style lang="scss">
+	footer{
+		height:100px;
+		padding-left:250px;
+		border-top:1px solid #ddd;
+		text-align:center;
+		p{
+			line-height:100px;
+		}
+	}
+</style>
