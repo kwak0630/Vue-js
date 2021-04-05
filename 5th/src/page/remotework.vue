d3ac7e676ba2ca4d4fcb65d829fd62192e5488ff
Sally kwak <39579205+kwak0630@users.noreply.github.com>
2021-04-05 14:30:33 +0900

    Add files via upload

---
+++ 5th/src/page/remotework.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<Lnb />
+		<div class="content">
+			<h2>원격근무 관리</h2>
+		</div>
+		<Footer />
+	</div>
+</template>
+
+<script>
+	import Lnb from "@/components/layout/Lnb";
+	import Footer from "@/components/layout/Footer";
+
+	export default {
+		name: "remotework",
+		components:	{
+			Lnb,
+			Footer
+		},
+	}
+</script>
